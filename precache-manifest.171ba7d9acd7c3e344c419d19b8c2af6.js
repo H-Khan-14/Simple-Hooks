58b731e85cae8b310899712d8dccbbca456feb26
H-Khan-14 <haseebkhan14@hotmail.com>
2021-09-09 16:58:12 -0400

    Updates

---
+++ precache-manifest.171ba7d9acd7c3e344c419d19b8c2af6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47300575b9229861aaa65042833b327d",
+    "url": "/Simple-Hooks/index.html"
+  },
+  {
+    "revision": "cb575f1bd9d804ee3f56",
+    "url": "/Simple-Hooks/static/js/2.63fbc30b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Simple-Hooks/static/js/2.63fbc30b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e7d4828db21fa2fd938",
+    "url": "/Simple-Hooks/static/js/main.60b0c853.chunk.js"
+  },
+  {
+    "revision": "0f31d455f62f104236fa",
+    "url": "/Simple-Hooks/static/js/runtime-main.d055e6d9.js"
+  }
+]);
